b17c75927cde2dc1be73fad92eb4ec3b1ec7e74b
Astrid <astrid.pinckers@minfin.fed.be>
2021-04-30 14:44:40 +0200

    finished codelab 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
+import { ProfileGalleryComponent } from './profile-gallery/profile-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileGalleryComponent,
   ],
   imports: [
     BrowserModule,
     LayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
